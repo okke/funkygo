25e88232f225fde1f20f6095b133a1880886cc6d
Okke van 't Verlaat <okke@fullinfo.com>
2023-12-31 16:00:47 +0100

    slice functions reorganized

---
+++ fs/slice.go
@@ -0,0 +1,22 @@
+package fs
+
+import "funcgo/fu"
+
+func FromSlice[T any](slice []T) Stream[T] {
+
+	return func() (T, Stream[T]) {
+		if len(slice) == 0 {
+			return fu.Zero[T](), nil
+		}
+		return slice[0], FromSlice(slice[1:])
+	}
+}
+
+func ToSlice[T any](stream Stream[T]) []T {
+
+	result := []T{}
+	for e, s := stream(); s != nil; e, s = s() {
+		result = append(result, e)
+	}
+	return result
+}
--- fs/stream.go
+++ fs/stream.go
@@ -4,25 +4,6 @@ import "funcgo/fu"
 
 type Stream[T any] func() (T, Stream[T])
 
-func FromSlice[T any](slice []T) Stream[T] {
-
-	return func() (T, Stream[T]) {
-		if len(slice) == 0 {
-			return fu.Zero[T](), nil
-		}
-		return slice[0], FromSlice(slice[1:])
-	}
-}
-
-func ToSlice[T any](stream Stream[T]) []T {
-
-	result := []T{}
-	for e, s := stream(); s != nil; e, s = s() {
-		result = append(result, e)
-	}
-	return result
-}
-
 func Peek[T any](stream Stream[T]) T {
 	value, _ := stream()
 	return value
